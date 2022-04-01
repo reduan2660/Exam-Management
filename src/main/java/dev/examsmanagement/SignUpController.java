95e2c3317bed8642a4ece90fbbc09dc7dc791c73
Alve Reduan <alve.reduan.09@gmail.com>
2022-04-01 14:17:53 +0600

    first commit

---
+++ src/main/java/dev/examsmanagement/SignUpController.java
@@ -0,0 +1,18 @@
+package dev.examsmanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    @FXML
+    public TextField signUpEmail;
+    public PasswordField signUpPassword;
+
+    @FXML
+    protected void signUp(){
+        System.out.println(signUpEmail.getText());
+        System.out.println(signUpPassword.getText());
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module dev.examsmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens dev.examsmanagement to javafx.fxml;
+    exports dev.examsmanagement;
+}
